d3fb5222eceffe92de28196ce9a98cb06fdb1c1f
Rusti111 <142669783+Rusti111@users.noreply.github.com>
2024-01-08 00:04:22 +0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useState } from 'react';
+import minus from './img/1-5.jpg';
+import first from './img/3.jpg';
+import second from './img/4.jpg';
+import third from './img/5.jpg';
+import last from './img/2.jpg';
+import './style.css';
 
-function App() {
+const App = () => {
+  const [num, setNum] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <h1>Счетчик</h1>
+
+      <h2>{num} {num === 15 ? 'max' : num === 0 ? 'min' : ''} </h2>
+
+      <button className='but' onClick={() => {
+        if (num < 15) setNum(num + 1);
+      }}>+</button>
+
+      <button className='but' onClick={() => {
+        if (num > -1) setNum(num - 1);
+      }}>-</button>
+
+      <button className='but' onClick={() => {
+        setNum(0)
+      }}>reset</button>
+
+      <button className='but' onClick={() => {
+        setNum(15)
+      }}>max</button>
+
+      <button className='but' onClick={() => {
+        if (num > 10) {
+          setNum(15);
+        } else {
+          setNum(num + 5)
+        }
+      }}>+5</button>
+
+      <button className='but' onClick={() => {
+        if (num < 5) {
+          setNum(0)
+        } else {
+          setNum(num - 5)
+        }
+      }}>-5</button>
+
+      <br />
+
+      <img onClick={() => {
+        if (num < 15) setNum(num + 1);
+      }} className='donkey' src={
+
+        num < 0
+          ? minus
+          : num < 5
+            ? first
+            : num < 10
+              ? second
+              : num < 15
+                ? third
+                : last
+      } alt="" />
+
     </div>
   );
 }
---
+++ src/Sed.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sed = () => {
+    return (
+        <div>
+            <h1>Hi.</h1>
+        </div>
+    );
+}
+
+export default Sed;
